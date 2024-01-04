ac726ec81574431876b6e9eb1d3c8890316f9ac2
kaleaniketcp <kaleaniketcp@gmail.com>
2024-01-05 00:28:18 +0530

    2nd commit

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -49,14 +49,52 @@ export const getHotel = async (req, res, next) => {
   }
 };
 
+//GET
+export const countByCity = async (req, res, next) => {
+  const cities = req.query.cities.split(",");
+  try {
+    const list = await Promise.all(
+      cities.map((city) => {
+        return Hotel.countDocuments({ city: city });
+      })
+    );
+    res.status(200).send(list);
+  } catch (err) {
+    next(err);
+  }
+};
+
+//GET
+export const countByType = async (req, res, next) => {
+  try {
+    const hotelCount = await Hotel.countDocuments({ type: "hotel" });
+    const apartmentCount = await Hotel.countDocuments({ type: "apartment" });
+    const resortCount = await Hotel.countDocuments({ type: "resort" });
+    const villaCount = await Hotel.countDocuments({ type: "villa" });
+    const cabinCount = await Hotel.countDocuments({ type: "cabin" });
+
+    res.status(200).send([
+      { type: "hotel", count: hotelCount },
+      { type: "apartments", count: apartmentCount },
+      { type: "resorts", count: resortCount },
+      { type: "villas", count: villaCount },
+      { type: "cabins", count: cabinCount },
+    ]);
+  } catch (err) {
+    next(err);
+  }
+};
+
 //GET ALL
 export const getHotels = async (req, res, next) => {
-  const id = req.params.id;
-
+  const { min, max, ...others } = req.query;
   try {
-    const hotels = await Hotel.find(id);
+    const hotels = await Hotel.find({
+      ...others,
+      cheapestPrice: { $gt: min || 1, $lt: max || 999 },
+    }).limit(req.query.limit);
     res.status(200).send(hotels);
-  } catch (error) {
-    next(error);
+  } catch (err) {
+    next(err);
   }
 };
--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -27,6 +27,7 @@ export const verifyUser = (req, res, next) => {
 
 export const verifyAdmin = (req, res, next) => {
   verifyToken(req, res, next, () => {
+    console.log(req.user);
     if (req.user.isAdmin) {
       next();
     } else {
